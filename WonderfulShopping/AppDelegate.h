9d55b252d55e4e6c11df851368b1ddb7aed1a332
laodaoduo <2295990355@qq.com>
2015-11-07 10:11:52 +0800

    Initial Commit

---
+++ WonderfulShopping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WonderfulShopping
+//
+//  Created by LWJ on 15-11-7.
+//  Copyright (c) 2015年 laodao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
